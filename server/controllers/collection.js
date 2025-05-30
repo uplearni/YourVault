835bcf61fc79b6b53fff54e06395c419ea611a99
uplearni <jiya07422@gmail.com>
2025-05-31 02:09:59 +0530

    collection route created

---
+++ server/controllers/collection.js
@@ -0,0 +1,114 @@
+const { Collection }= require("../models/collection");
+const mongoose= require("mongoose");
+const {throwError}= require("../utils/helper");
+
+
+exports.getCollections=async (req,res,next)=>{
+   const userId=req.userId;
+   try{
+    const collection=await Collection.find({userId:userId});
+    res.status(200).json({
+        message:"success",
+        collection,
+    })
+    }catch(err){
+     next(err);
+    }
+}
+
+exports.createCollection=async (req,res,next)=>{
+    const cname=req.body.cname;
+    const description=req.body.description;
+    
+    try{
+        if(!cname){
+            throwError("Please enter a collection name",422);
+        }
+        const collection=new Collection({cname,description,createdBy:req.userId});
+        const result= await collection.save();
+        res.status(200).json({
+            message:"Collection created Successfully",
+            data:{
+                id:result._id.toString(),
+                collection:result.cname
+            }
+        });
+    }catch(err){
+        next(err);
+    }
+}
+
+exports.getCollectionById=async (req,res,next)=>{
+    const collectionId = req.params.collectionId; // or rename this to collectionId for clarity
+
+  try {
+    if (!collectionId) throwError("Invalid collection ID provided.", 422);
+
+    const data = await Collection.aggregate([
+      {
+        $match: { _id: mongoose.Types.ObjectId(collectionId) },
+      },
+      {
+        $lookup: {
+          from: "items", // Collection name for Item model (Mongo auto-pluralizes it)
+          let: { collectionId: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: { $eq: ["$collectionId", "$$collectionId"] },
+              },
+            },
+          ],
+          as: "items",
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      message: "Collection fetched successfully",
+      data: data[0], 
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateCollection=async (req,res,next)=>{
+    const cname=req.body.cname;
+    const description=req.body.description;
+    const collectionId=req.params.collectionId;
+    const userId=req.userId;
+
+    try{
+        const result=await Collection.findOneAndUpdate(
+            {_id:collectionId,userId:userId},
+            {cname,description},
+            {new:true},
+        )
+
+        if(!result) throwError("Collection not found",404);
+
+        res.status(200).json({
+            message:"Collection found",
+            data:result,
+        })
+    }catch(err){
+        next(err);
+    }
+}
+
+exports.deleteCollection=async (req,res)=>{
+    const collectionId=req.params.collectionId;
+    const userId=req.userId;
+
+    try{
+        const result=await Collection.deleteOne({_id:collectionId,createdBy:userId});
+        if(!result) throwError("no collection found",422);
+        
+        res.status(200).json({
+            message:"Collection delete successfully",
+        });
+    }catch(err){
+        next(err);
+    }
+}
--- server/routes/collection.js
+++ server/routes/collection.js
@@ -1,5 +1,12 @@
 const express = require('express');
-
+const collectionController=require("../controllers/collection")
+const isAuth =require("../middleware/is-auth");
 const router = express.Router();
+
+router.get("/",isAuth,collectionController.getCollections);
+router.post("/",isAuth,collectionController.createCollection);
+router.get("/:collectionId",isAuth,collectionController.getCollectionById);
+router.put("/:collectionId",isAuth,collectionController.updateCollection);
+router.delete("/:collectionId",isAuth,collectionController.deleteCollection);
 
 module.exports = router;
