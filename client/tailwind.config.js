c5fab40345bb69a142c92aa1ad3488bedb0c8d8e
uplearni <jiya07422@gmail.com>
2025-06-15 22:56:33 +0530

    added latest version of tailwind/vite

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,10 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', //this tells tailwind to look for a dark class 
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
